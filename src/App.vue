2ea50f7b3d33a451a819238e2d92b05b71e54fb7
freaklucas <lucas.oliveira.freak@gmail.com>
2021-09-16 02:29:20 -0300

    setup inicial

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="md-layout">
+    <div class="md-layout-item md-size-33" v-for="quadrinho in quadrinhos" :key="quadrinho.id">
+      <quadrinho
+        :titulo="quadrinho.title"
+        :descricao="quadrinho.description"
+        :imagem="getImagem(quadrinho)"
+    ></quadrinho>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import MarvelApi from '@/services/MarvelAPI';
+import Quadrinho from '@/components/Card';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Quadrinho
+  },
+  data() {
+    return {
+      quadrinhos: []
+    }
+  },
+  created() {
+    var self = this
+    MarvelApi.getAllComics(10, comics => {
+      self.quadrinhos = comics.data.data.results;
+    })
+  },
+  methods: {
+    getImagem: function(quadrinho) {
+      if (quadrinho.images.length) {
+        return quadrinho.images[0].path + '/portrait_medium.jpg';
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
+// import router from './router';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+
+Vue.use(VueRouter);
+
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+});
